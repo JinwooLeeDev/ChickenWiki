a787b0e050ed939251ff02128a7447c470790ce3
I3abel <wlsdn48971@gmail.com>
2026-02-13 15:10:17 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/ChickinWiki/ChickinWiki/ChickinWikiApplication.java
@@ -0,0 +1,13 @@
+package com.ChickinWiki.ChickinWiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickinWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickinWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChickinWiki/ChickinWiki/ChickinWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChickinWiki.ChickinWiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickinWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
