6a31fa810b9cbd56f44838cf6ca00103589ef974
Jinwoo <jinwoo_dev@naver.com>
2026-02-23 21:29:12 +0900

    refactor : rename Chickin -> Chicken across project

--- src/main/java/com/ChickenWiki/ChickenWiki/ChickenWikiApplication.java
+++ src/main/java/com/ChickenWiki/ChickenWiki/ChickenWikiApplication.java
@@ -1,13 +1,13 @@
-package com.ChickinWiki.ChickinWiki;
+package com.ChickenWiki.ChickenWiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChickinWikiApplication {
+public class ChickenWikiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChickinWikiApplication.class, args);
+		SpringApplication.run(ChickenWikiApplication.class, args);
 	}
 
 }
